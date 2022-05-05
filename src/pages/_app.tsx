e0e5281ca7037db94d89136222142b15c33d0f0c
tonirah <tonirah@posteo.de>
2022-05-05 16:18:18 +0200

    feat: disable SSR to prevent hydration errors
    Using localState for the progress system caused
    hydration errors due to the difference between SSR and CSR.
    SSR is not used in production because app is completely
    based on SSG (static export).
    This disables SSR for the complete app in all modes
    including development.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
 import { AppProps } from 'next/app';
+import dynamic from 'next/dynamic';
 import '@/styles/global.css';
 import '@/common/utils/whyDidYouRender';
-import ProgressProvider from '@/common/hooks/ProgressProvider';
+
+const ProgressProvider = dynamic(
+  () => import(`@/common/hooks/ProgressProvider`),
+  { ssr: false },
+);
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
