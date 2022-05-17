acfa8e1b72e6fc290790d2360d52ed8320067869
tonirah <tonirah@posteo.de>
2022-05-17 14:01:27 +0200

    feat: add info link to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import { useContext } from 'react';
 import { ProgressContext } from '@/common/hooks/';
 import { TaskProgressIcon } from '@/common/components';
+import { InformationCircleIcon } from '@heroicons/react/outline';
 
 export default function Home({ allTasks }: { allTasks: ITask[] }) {
   const {
@@ -21,9 +22,16 @@ export default function Home({ allTasks }: { allTasks: ITask[] }) {
       </Head>
 
       <div className="container mx-auto text-center py-28 px-2">
-        <h1 className="font-mono text-6xl font-bold tracking-wider underline text-primary-focus decoration-warning leading-relaxed mb-2">
+        <h1 className="font-mono text-6xl font-bold tracking-wider underline text-primary-focus decoration-warning leading-relaxed -mb-3">
           SOC Asklepios
         </h1>
+        <Link href={`/info`}>
+          <a className="btn btn-link gap-1">
+            <InformationCircleIcon className="h-6 w-6 text-info" />
+            Story, Mission, Refactorings, Code Smells
+          </a>
+        </Link>
+        <div className="divider max-w-5xl mx-auto mb-8"></div>
 
         <div className="flex justify-center items-center gap-3 mb-8">
           <div className="stats flex-none">
@@ -55,30 +63,32 @@ export default function Home({ allTasks }: { allTasks: ITask[] }) {
             ></progress>
           </div>
         </div>
-        <div className="divider max-w-5xl mx-auto mb-10"></div>
+        <div className="divider max-w-5xl mx-auto mb-8"></div>
 
-        <h2 className="font-mono text-2xl font-bold tracking-wider underline text-secondary decoration-warning mb-5">
-          Tasks:
+        <h2 className="font-mono text-2xl font-bold tracking-wider underline text-secondary decoration-warning mb-4">
+          Tasks
         </h2>
         <div className="w-max mx-auto mb-8">
           {allTasks.map((task) => {
             return (
-              <div key={task.uuid} className="flex flex-row items-center">
-                <TaskProgressIcon
-                  taskProgress={getTaskProgress(task.uuid)}
-                  className="h-8 w-8"
-                />
-                <Link href={`/tasks/${task.uuid}`}>
-                  <a className="btn btn-link px-1">{task.name}</a>
-                </Link>
-              </div>
+              <Link key={task.uuid} href={`/tasks/${task.uuid}`}>
+                <a className="btn btn-link block w-fit">
+                  <div key={task.uuid} className="flex flex-row items-center">
+                    <TaskProgressIcon
+                      taskProgress={getTaskProgress(task.uuid)}
+                      className="h-8 w-8 mr-1"
+                    />
+                    {task.name}
+                  </div>
+                </a>
+              </Link>
             );
           })}
         </div>
-        <div className="divider max-w-5xl mx-auto mb-10"></div>
+        <div className="divider max-w-5xl mx-auto mb-8"></div>
 
         <button
-          className="btn btn-outline btn-accent"
+          className="btn btn-sm btn-outline btn-accent"
           onClick={() => resetProgress()}
         >
           Fortschritt zurücksetzen
