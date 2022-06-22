d9bbbadc0add1335fd8d20e3e92f11dffcddbb13
tonirah <tonirah@posteo.de>
2022-06-22 16:51:35 +0200

    feat: add 2D spaceship dummy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,108 +30,113 @@ export default function Home({ allTasks }: { allTasks: ITask[] }) {
           {title}
         </h1>
 
-        <div className="flex flex-col xl:flex-row gap-3 items-center w-full bg-neutral border-neutral border rounded-box shadow-xl mb-12">
-          <div className="flex flex-col xl:w-0 flex-auto">
-            <div className="text-neutral-content pt-8 pb-3 px-8">
-              <p className="font-mono">
-                <span className="font-bold">„Im Jahr 2094.</span> Du befindest
-                dich auf einer interplanetaren Reise, und warst bis gerade eben
-                im Hyperschlaf. Die AI (SOLID) des Schiffes 'SOC Asklepios' hat
-                dich geweckt. Du musst ihr helfen, die Boardsoftware zu prüfen
-                und zu refaktorisieren, um sicher auf dem Planet 'Yagni' landen
-                zu können.<span className="font-bold">“</span>
-              </p>
-              <Link href={`/info`}>
-                <a className="btn btn-link">
-                  <InformationCircleIcon className="h-6 w-6 mr-1 text-info" />
-                  Story, Mission, Refactorings
+        <div className="bg-neutral rounded-box shadow-xl mb-12 text-neutral-content pt-8 pb-3 px-8">
+          <p className="font-mono">
+            <span className="font-bold">„Im Jahr 2094.</span> Du befindest dich
+            auf einer interplanetaren Reise, und warst bis gerade eben im
+            Hyperschlaf. Die AI (SOLID) des Schiffes 'SOC Asklepios' hat dich
+            geweckt. Du musst ihr helfen, die Boardsoftware zu prüfen und zu
+            refaktorisieren, um sicher auf dem Planet 'Yagni' landen zu können.
+            <span className="font-bold">“</span>
+          </p>
+          <Link href={`/info`}>
+            <a className="btn btn-link">
+              <InformationCircleIcon className="h-6 w-6 mr-1 text-info" />
+              Story, Mission, Refactorings
+            </a>
+          </Link>
+        </div>
+
+        <div className="mx-auto w-full xl:w-4/5 2xl:w-2/3 mb-12">
+          <div className="flex w-full gap-2 justify-center items-end">
+            <div className="text-left w-1/2">
+              <Link
+                href={`/tasks/${getRandomTaskId(Category.CentralFlightSystem)}`}
+              >
+                <a>
+                  <div className="opacity-60 mb-1">
+                    {Category.CentralFlightSystem}
+                  </div>
+                  <progress
+                    className="block progress progress-accent mb-4"
+                    value={getCategoryProgressPercentage(
+                      Category.CentralFlightSystem,
+                    )}
+                    max="100"
+                  ></progress>
+                </a>
+              </Link>
+            </div>
+            <div className="text-right w-1/2">
+              <Link
+                href={`/tasks/${getRandomTaskId(
+                  Category.VentilationThermalControl,
+                )}`}
+              >
+                <a>
+                  <div className="opacity-60 mb-1">
+                    {Category.VentilationThermalControl}
+                  </div>
+                  <progress
+                    className="block progress progress-secondary mb-4"
+                    value={getCategoryProgressPercentage(
+                      Category.VentilationThermalControl,
+                    )}
+                    max="100"
+                  ></progress>
                 </a>
               </Link>
             </div>
           </div>
-          <div className=" flex flex-col xl:w-0 flex-auto bg-base-300 w-full">
-            <div className="flex justify-evenly items-center gap-3 py-4 px-2">
-              <div className="stats flex-none bg-inherit">
-                <div className="stat">
-                  <div className="stat-title">Gesamtpunkte</div>
-                  <div className="stat-value">{getTotalScore()}</div>
-                </div>
-              </div>
-              <div>
-                <Link
-                  href={`/tasks/${getRandomTaskId(
-                    Category.CentralFlightSystem,
-                  )}`}
-                >
-                  <a>
-                    <div className="opacity-60 mb-1">
-                      {Category.CentralFlightSystem}
-                    </div>
-                    <progress
-                      className="block progress progress-accent mb-4"
-                      value={getCategoryProgressPercentage(
-                        Category.CentralFlightSystem,
-                      )}
-                      max="100"
-                    ></progress>
-                  </a>
-                </Link>
-                <Link
-                  href={`/tasks/${getRandomTaskId(
-                    Category.VentilationThermalControl,
-                  )}`}
-                >
-                  <a>
-                    <div className="opacity-60 mb-1">
-                      {Category.VentilationThermalControl}
-                    </div>
-                    <progress
-                      className="block progress progress-secondary mb-4"
-                      value={getCategoryProgressPercentage(
-                        Category.VentilationThermalControl,
-                      )}
-                      max="100"
-                    ></progress>
-                  </a>
-                </Link>
-                <Link
-                  href={`/tasks/${getRandomTaskId(
-                    Category.CrewHealthHypersleep,
-                  )}`}
-                >
-                  <a>
-                    <div className="opacity-60 mb-1">
-                      {Category.CrewHealthHypersleep}
-                    </div>
-                    <progress
-                      className="block progress progress-success  mb-4"
-                      value={getCategoryProgressPercentage(
-                        Category.CrewHealthHypersleep,
-                      )}
-                      max="100"
-                    ></progress>
-                  </a>
-                </Link>
+          <div className="bg-base-300 text-base-content border-primary border-4 border-double shadow-lg shadow-primary rounded-box mx-auto w-full lg:w-3/4 aspect-video flex flex-col justify-evenly items-center mb-2">
+            <div className="font-mono text-4xl">{`<Spaceship>`}</div>
+            <div>
+              <div className="opacity-60">Gesamtpunkte</div>
+              <div className="font-bold text-6xl">{getTotalScore()}</div>
+            </div>
+            <div className="font-mono text-4xl">{`</Spaceship>`}</div>
+          </div>
+          <div className="flex w-full gap-2 justify-center items-start">
+            <div className="text-left w-1/2">
+              <Link
+                href={`/tasks/${getRandomTaskId(
+                  Category.CrewHealthHypersleep,
+                )}`}
+              >
+                <a>
+                  <progress
+                    className="block progress progress-success mt-4 mb-1"
+                    value={getCategoryProgressPercentage(
+                      Category.CrewHealthHypersleep,
+                    )}
+                    max="100"
+                  ></progress>
+                  <div className="opacity-60">
+                    {Category.CrewHealthHypersleep}
+                  </div>
+                </a>
+              </Link>
+            </div>
+            <div className="text-right w-1/2">
+              <Link
+                href={`/tasks/${getRandomTaskId(
+                  Category.CrewHealthHypersleep,
+                )}`}
+              >
                 {/* TODO: use Category.SpaceRadiationProtection when it gets task */}
-                <Link
-                  href={`/tasks/${getRandomTaskId(
-                    Category.CrewHealthHypersleep,
-                  )}`}
-                >
-                  <a>
-                    <div className="opacity-60 mb-1">
-                      {Category.CrewHealthHypersleep}
-                    </div>
-                    <progress
-                      className="block progress progress-primary mb-4"
-                      value={getCategoryProgressPercentage(
-                        Category.CrewHealthHypersleep,
-                      )}
-                      max="100"
-                    ></progress>
-                  </a>
-                </Link>
-              </div>
+                <a>
+                  <progress
+                    className="block progress progress-primary mt-4 mb-1"
+                    value={getCategoryProgressPercentage(
+                      Category.CrewHealthHypersleep,
+                    )}
+                    max="100"
+                  ></progress>
+                  <div className="opacity-60">
+                    {Category.CrewHealthHypersleep}
+                  </div>
+                </a>
+              </Link>
             </div>
           </div>
         </div>
