11a1c96d0c69a35a826a527a883dc8f72364fee4
tonirah <tonirah@posteo.de>
2022-07-13 12:05:11 +0200

    feat: add svg favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,16 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import { SparklesIcon } from '@heroicons/react/outline';
+import { renderToStaticMarkup } from 'react-dom/server';
 
 export default function Document() {
+  const faviconStringUrl = encodeURIComponent(
+    renderToStaticMarkup(<SparklesIcon style={{ stroke: `#fc56c2` }} />),
+  );
   return (
     <Html lang="de">
-      <Head />
+      <Head>
+        <link rel="icon" href={`data:image/svg+xml,${faviconStringUrl}`} />
+      </Head>
       <body>
         <Main />
         <NextScript />
