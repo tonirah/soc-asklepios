3ea1b71705b1f802bbe0db16509e0c08e093a63e
tonirah <tonirah@posteo.de>
2022-07-14 11:51:08 +0200

    feat: add more info on how to use the game

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,13 +1,25 @@
 import { Refactoring } from '@/modules/tasks';
 import { parseRequiredInt } from '@/common/utils/';
 import { ArrowCircleLeftIcon } from '@heroicons/react/outline';
-import { Footer, Navbar } from '@/common/components';
+import {
+  CodeBlock,
+  Footer,
+  Navbar,
+  TaskProgressIcon,
+} from '@/common/components';
 import Link from 'next/link';
 import Head from 'next/head';
+import { TaskProgress } from '@/common/hooks';
 
 const MIN_CHARACTERS_FOR_COMBOBOX = parseRequiredInt(
   process.env.MIN_CHARACTERS_FOR_COMBOBOX,
 );
+
+const sampleCode = `interface ISample {
+  void sampleMethod();
+
+  // ...
+}`;
 
 export default function Info() {
   const title = `Story, Mission, Refactorings`;
@@ -20,7 +32,7 @@ export default function Info() {
 
       <Navbar />
 
-      <div className="container mx-auto text-center pt-12 pb-32 px-2">
+      <div className="container mx-auto text-left pt-12 pb-32 px-2">
         <h1 className="font-mono text-3xl font-bold tracking-wider underline text-primary-focus decoration-accent text-center mb-4">
           {title}
         </h1>
@@ -47,13 +59,96 @@ export default function Info() {
               </strong>
             </p>
             <p>
-              Für jede Codestelle musst du passende Refactorings eingeben.
-              Mögliche Eingaben sind weiter unten auf dieser Seite aufgelistet.
+              <strong>
+                Für jede Codestelle musst du passende Refactorings eingeben
+              </strong>
+              . Mögliche Eingaben sind weiter unten auf dieser Seite
+              aufgelistet.
             </p>
             <p>
-              Sobald du mindestens{` `}
-              {MIN_CHARACTERS_FOR_COMBOBOX} Buchstaben eingegeben hast werden
-              dir mögliche Eingaben vorgeschlagen (Combobox mit Autosuggest).
+              Als Input-Elemente werden <strong>Comboboxen</strong> verwendet,
+              diese sind eine Kombination Textfeld und Auswahlliste.{` `}
+              <strong>Es gibt 2 Aufgabenarten:</strong>
+            </p>
+            <ol>
+              <li>
+                Alle Auswahl-Optionen werden beim Klick in das Textfeld
+                angezeigt (Multiple-Choice).
+              </li>
+              <li>
+                Es müssen mindestens {MIN_CHARACTERS_FOR_COMBOBOX} Buchstaben
+                eingegeben werden, damit mögliche Auswahl-Optionen angezeigt
+                werden.
+              </li>
+            </ol>
+            <p>
+              Über den Schalter <strong>„Codestellen hervorheben“</strong>
+              {` `}
+              können optional verdächtige Zeilen hervorgehoben werden:
+            </p>
+            <div className="bg-base-200 rounded-box">
+              <CodeBlock
+                code={sampleCode}
+                highlightedLines="2"
+                isLineHintActive={true}
+              />
+            </div>
+            <p>
+              <strong>Die Aufgaben sind auf zwei Wegen zugänglich:</strong>
+            </p>
+            <ol>
+              <li>
+                Beim Klick auf einen Bereich des Raumschiffs wird zufällig eine
+                (noch nicht gelöste) Aufgabe angezeigt.
+              </li>
+              <li>
+                Über den Menüpunkt „Liste kritischer Codestellen“ können
+                Aufgaben direkt ausgewählt werden.
+              </li>
+            </ol>
+            <p>
+              In der „Liste kritischer Codestellen“ ist der{` `}
+              <strong>Status der Aufgaben</strong> ersichtlich: Offen (
+              {
+                <TaskProgressIcon
+                  taskProgress={undefined}
+                  className="h-7 inline-block"
+                />
+              }
+              ), begutachtet (
+              {
+                <TaskProgressIcon
+                  taskProgress={TaskProgress.Visited}
+                  className="h-7 inline-block"
+                />
+              }
+              ) und gelöst (
+              {
+                <TaskProgressIcon
+                  taskProgress={TaskProgress.Solved}
+                  className="h-7 inline-block"
+                />
+              }
+              ).
+            </p>
+            <p>
+              Über den Menüpunkt{` `}
+              <strong
+                className="bg-base-300 text-base-content p-2 rounded-box"
+                data-theme="light"
+              >
+                „Helles Design“
+              </strong>
+              {` `}
+              bzw.{` `}
+              <strong
+                className="bg-base-300 text-base-content p-2 rounded-box"
+                data-theme="dark"
+              >
+                „Dunkles Design“
+              </strong>
+              {` `}
+              kann das Erscheinungsbild entsprechend eingestellt werden.
             </p>
             <h3 className="text-secondary underline decoration-accent">
               Details
@@ -92,18 +187,16 @@ export default function Info() {
             >
               Liste von Refactorings
             </h2>
-            <div className="text-left">
-              <p>Die folgenden Refacotrings kommen in „SOC Asklepios“ vor:</p>
-              <ol>
-                {Object.values(Refactoring)
-                  .sort()
-                  .map((refactoring) => (
-                    <li className="leading-relaxed" key={refactoring}>
-                      {refactoring}
-                    </li>
-                  ))}
-              </ol>
-            </div>
+            <p>Die folgenden Refacotrings kommen in „SOC Asklepios“ vor:</p>
+            <ol>
+              {Object.values(Refactoring)
+                .sort()
+                .map((refactoring) => (
+                  <li className="leading-relaxed" key={refactoring}>
+                    {refactoring}
+                  </li>
+                ))}
+            </ol>
           </div>
         </div>
         <div className="mt-4 text-center">
